95040205d6d553f854b1599a80e4ed1c77f27e49
Fernando González <rmfernando@outlook.com>
2020-01-15 16:51:08 -0600

    second try...

--- problema0.py
+++ problema0.py
@@ -10,6 +10,9 @@ num60 = ["secenta"]
 num70 = ["setenta"]
 num80 = ["ochenta"]
 num90 = ["noventa"]
+cien = ["ciento"]
+sientos = ["ientos"]
+
 
 x = int(input("Ingrese un numero porfavor: "))
 
@@ -47,8 +50,26 @@ elif (x > 80 and x < 90):
     print(num80[0] + " y " + num[x - 81])
 elif (x == 90):
     print(num90[0])
-elif (x > 90 and x < 1000):
+elif (x > 90 and x < 100):
     print(num90[0] + " y " + num[x - 91])
+elif (x == 100):
+    print("cien")
+elif (x > 100 and x < 110):
+    print(cien[0] + num[x - 101])
+elif (x > 110 and x < 120):
+    print(cien[0] + " y " + num2[x - 111])
+elif (x == 120):
+    print(cien[0] + num20[0])
+elif (x > 120 and x < 130):
+    print(cien[0] + " y " + num20[x - 121] + " y " + num[x - 121])
+elif (x == 130):
+    print(cien[0] + " y " + num30[0])
+elif (x > 130 and x < 140):
+    print(cien[0] + " y " + num30[x - 121] + " y " + num[x - 131])
+elif (x == 140):
+    print(cien[0] + " y " + num40[0])
+elif (x > 140 and x < 150):
+    print(cien[0] + " y " + num40[x - 121] + " y " + num[x - 141])
 elif (x == 1000):
     print("mil")
 else:
